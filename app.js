d4931b3e546f6d75e17622d0903d984ab6f2a289
Shabaz100 <152277607+Shabaz100@users.noreply.github.com>
2024-01-29 22:07:29 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+document.getElementById(`h1`).innerText = `MY NAME IS SHABAZ`;
+document.getElementById(`h1`).style.color =`Green`;
+document.getElementById(`h2`).innerText = `MY NAME IS SHABAZ`;
+document.getElementById(`h2`).style.color =`red`;
+document.getElementById(`h3`).innerText = `MY NAME IS SHABAZ`;
+document.getElementById(`h3`).style.color =`Green`;
+
+document.querySelector(`button`);
+addEventListener(`click`,function(){
+document.querySelector(`h1`).innerText = `Hello Bangladesh!`;
+document.querySelector(`h1`).style.color = `Green`;
+document.querySelector(`h2`).innerText = `Hello Bangladesh!`;
+document.querySelector(`h2`).style.color = `red`;
+document.querySelector(`h3`).innerText = `Hello Bangladesh!`;
+document.querySelector(`h2`).style.color = `Green`;
+
+})
